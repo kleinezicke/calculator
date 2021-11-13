371b22f8d7362d06ccfa5f1f069f59114e3fd95f
kleinezicke <julia.neuburger@24speed.at>
2021-11-13 21:36:51 +0100

    added operate function

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const addition = (x, y) => x += y;
 const subtraction = (x, y) => x -= y;
 const multiplication = (x, y) => x *= y;
 const division = (x, y) => x / y;
+
+function operate(num1, num2, operator) {
+    let result;
+    switch (operator) {
+        case "add":
+            result = addition(num1, num2);
+            return result;
+        case "sub":
+            result = subtraction(num1, num2);
+            return result;
+        case "mult":
+            result = multiplication(num1, num2);
+            return result;
+        case "div":
+            result = division(num1, num2);
+            return result;
+    }
+}
