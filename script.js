f213c428f27142f4be83e8320d5210b9381c2602
kleinezicke <julia.neuburger@24speed.at>
2021-11-13 22:19:49 +0100

    added number input with buttons

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+let number = "";
+let operator;
+
 const addition = (x, y) => x += y; 
 const subtraction = (x, y) => x -= y;
 const multiplication = (x, y) => x *= y;
 const division = (x, y) => x / y;
+
+const displayDiv = document.querySelector("#display");
+const numButton = document.querySelectorAll(".button")
+
+const buttons = () => {
+    numButton.forEach(element => {
+        element.addEventListener("click", function(e) {
+            addNum(element.id);
+        })
+    });
+}
+
+buttons();
 
 function operate(num1, num2, operator) {
     let result;
@@ -19,4 +35,11 @@ function operate(num1, num2, operator) {
             result = division(num1, num2);
             return result;
     }
+}
+
+function addNum(num) {
+    
+    number += num;
+    displayDiv.textContent = number;
+
 }
